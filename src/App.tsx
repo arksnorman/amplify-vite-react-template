6fc233b926d46c8b21ee1f8b97d11b2a8750de53
Norman Kidhoma <arksnorman@gmail.com>
2025-03-19 19:43:39 +0000

    Update  auth

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,11 @@ import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { FileUploader } from '@aws-amplify/ui-react-storage';
 import '@aws-amplify/ui-react/styles.css';
-import { Authenticator } from '@aws-amplify/ui-react';
-import type { AppProps } from 'next/app';
-
-
+import { useAuthenticator } from '@aws-amplify/ui-react';
 
 const client = generateClient<Schema>();
 
-function App({ Component, pageProps }: AppProps) {
+function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   useEffect(() => {
@@ -22,19 +19,12 @@ function App({ Component, pageProps }: AppProps) {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
+  
+  const { signOut } = useAuthenticator();
 
   return (
     <main>
-       <Authenticator>
-        {({ signOut, user }) => (
-          <main>
-            <h1>Hello {user?.username}</h1>
-            <button onClick={signOut}>Sign out</button>
-            <Component {...pageProps} />
-          </main>
-        )}
-      </Authenticator>
-      
+      <button onClick={signOut}>Sign out</button>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
